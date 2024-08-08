dc7938c5914e7cd16a8403e60f185b895efd9dd9
Kyle Roche <kyleroche@users.noreply.github.com>
2024-08-07 21:43:26 -0700

    adding read (no tests)

--- griptape/tools/google_gmail/tool.py
+++ griptape/tools/google_gmail/tool.py
@@ -7,7 +7,7 @@ from email.message import EmailMessage
 from attrs import define, field
 from schema import Literal, Schema
 
-from griptape.artifacts import ErrorArtifact, InfoArtifact
+from griptape.artifacts import ErrorArtifact, InfoArtifact, ListArtifact, TextArtifact
 from griptape.tools import BaseGoogleClient
 from griptape.utils.decorators import activity
 
@@ -55,3 +55,54 @@ class GoogleGmailClient(BaseGoogleClient):
         except Exception as error:
             logging.error(error)
             return ErrorArtifact(f"error creating draft email: {error}")
+
+    @activity(
+        config={
+            "description": "Can be used to read emails from the GMail inbox",
+            "schema": Schema(
+                {
+                    Literal("max_results", description="maximum number of emails to fetch"): int,
+                    Literal("page_token", description="page token to continue from previous result"): int,
+                    Literal("query", description="google query string to be passed to messages().list method"): str
+                },
+            ),
+        }
+    )
+    def read_emails(self, params: dict) -> ListArtifact | ErrorArtifact:
+        values = params["values"]
+        max_results = values.get("max_results", 10)
+        query_string = values.get("query")
+
+        try:
+            service = self._build_client(
+                scopes=["https://www.googleapis.com/auth/gmail.readonly"],
+                service_name="gmail",
+                version="v1",
+                owner_email=self.owner_email,
+            )
+
+            # Query for messages in the inbox where the user is in the To: field
+            # query = "in:inbox to:me"
+            query = query_string
+            results = service.users().messages().list(userId="me", maxResults=max_results, q=query).execute()
+            messages = results.get("messages", [])
+
+            emails = []
+            for message in messages:
+                msg = service.users().messages().get(userId="me", id=message["id"]).execute()
+                headers = {header["name"]: header["value"] for header in msg["payload"]["headers"]}
+                email_info = {
+                    "from": headers.get("From"),
+                    "date": headers.get("Date"),
+                    "subject": headers.get("Subject"),
+                }
+                emails.append(email_info)
+
+            next_page_token = results.get("nextPageToken")
+            # uh...
+
+            return ListArtifact([TextArtifact(email) for email in emails])
+
+        except Exception as error:
+            logging.error(error)
+            return ErrorArtifact(f"error reading emails: {error}")
